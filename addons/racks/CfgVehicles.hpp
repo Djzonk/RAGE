2bc507465a8454c8f28934630fa2bc63c339a190
DjZonk <dannyjupp24@gmail.com>
2016-09-29 23:40:21 -0500

    ace interaction works

--- addons/racks/CfgVehicles.hpp
+++ addons/racks/CfgVehicles.hpp
@@ -1,8 +1,9 @@
 class CfgVehicles {
-    class Items_base_f;
-    class RAGE_Rack45 : Items_base_f {
+    class NATO_Box_Base;
+    class RAGE_Rack45 : NATO_Box_Base {
         scope = 2;
         scopeCurator = 2;
+        priority = 5
         displayname = CSTRING(Displayname);
         editorCategory = "EdCat_Supplies";
         model = QPATHTOF(data\RAGE_Rack45.p3d);
@@ -14,24 +15,19 @@ class CfgVehicles {
             };
         };
         class ACE_Actions {
-            class ACE_MainAction {
-                selection = "";
-                distance = 5;
-                class Open_Door {
-                    displayname = CSTRING(Opendoor);
-                    condition = true;
-                    statement = QUOTE(_this animate [QUOTE(left1_rotation), 1]);
-                    distance = 5;
-                    selection = "";
-
-                };
-                class Close_Door {
-                    displayname = CSTRING(Closedoor);
-                    condition = true;
-                    statement = QUOTE(_this animate [QUOTE(left1_rotation), 0]);
-                    distance = 5;
-                    selection = "";
-                };
+            class Open_Door {
+                displayname = CSTRING(Opendoor);
+                condition = QUOTE(_target animationPhase QUOTE(QUOTE(left1_rotation))  == 0 && alive _target);
+                statement = QUOTE(_target animate [ARR_2(QUOTE(QUOTE(left1_rotation)), 1)]);
+                selection = "actionPoint";
+                distance = 2;
+            };
+            class Close_Door {
+                displayname = CSTRING(Closedoor);
+                condition = QUOTE(_target animationPhase QUOTE(QUOTE(left1_rotation))  == 1 && alive _target);
+                statement = QUOTE(_target animate [ARR_2(QUOTE(QUOTE(left1_rotation)), 0)]);
+                selection = "actionPoint";
+                distance = 2;
             };
         };
     };
